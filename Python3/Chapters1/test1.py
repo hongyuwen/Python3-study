412c49e18eb6c067e1f0293918e0567fb5f49795
Aust <hongyuwen@yeah.net>
2017-10-20 16:35:16 +0800

    Update test1.py

--- Python3/Chapters1/test1.py
+++ Python3/Chapters1/test1.py
@@ -10,3 +10,18 @@ t1 = int(72)
 t2 = int(85)
 t3 = t1 / t2
 print("%.1f" % (t1 / t2))
+
+#计算体重的BMI
+higt=1.72
+height=77.5
+bmi=height / (higt * higt)
+print(bmi)
+
+if bmi < 18.5:
+	print("过轻")
+elif (bmi >= 18.5 and bmi < 25):
+	print("正常")
+elif bmi >= 25 and bmi < 28:
+	print("过重")
+else:
+	print("no")
